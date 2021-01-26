4690417ad257b3d6d5db58d03978d77422eadbac
paduck210 <hjyu1408@gmail.com>
2021-01-25 23:14:59 -0500

    Update

--- background.js
+++ background.js
@@ -1,16 +1,18 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({hide: true}, function() {
-      console.log("Hide image is on");
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ hide: false }, function () {
+    console.log("Hide image is on");
   });
+});
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.quora.com'},
-      })
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.addRules([
+    {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostContains: ".linkedin.com" },
+        }),
       ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-
+      actions: [new chrome.declarativeContent.ShowPageAction()],
+    },
+  ]);
+});
